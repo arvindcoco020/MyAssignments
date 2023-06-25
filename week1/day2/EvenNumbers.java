f7cc5c1f5de284a211c430b658e9f59d2f4c6fbf
arvindcoco020 <137687618+arvindcoco020@users.noreply.github.com>
2023-06-25 15:52:58 +0530

    Add files via upload
    Week1

---
+++ week1/day2/EvenNumbers.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//int i = 20;
+		System.out.println("Even Numbers from 20 to 30");
+		
+		for (int i=20; i<=30; i++) 
+		{
+			if (i%2 == 0) 
+			{
+				System.out.println(i);
+			}
+			
+			}
+		}
+	}
+
+
